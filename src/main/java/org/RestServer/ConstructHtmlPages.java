9a59c8b60191eaf59c4098265aefa9b03c6b7758
Roland Labruyere <roland.labruyere@gmail.com>
2026-07-17 08:34:48 +0200

    final commit; added several files

--- src/main/java/org/RestServer/ConstructHtmlPages.java
+++ src/main/java/org/RestServer/ConstructHtmlPages.java
@@ -16,29 +16,9 @@ public class ConstructHtmlPages {
         String htmlPage = rs.getString("InlineHtml");
 
         //htmlPage = setActivePage(htmlPage, tabItem);
-        fps.writeToLog(htmlPage);
+        //fps.writeToLog(htmlPage);
         myConn.close();
         return htmlPage;
     }
-    public String getCssPage(String tabItem) throws Throwable {
-        //String[] placeholders;
-        myConn.connect();
-        String css = myConn.fetchSql("select * from tb110_css_files where id = ?", tabItem, "cssFile");
-
-        myConn.close();
-        return css;
-    }
-    // private String setActivePage(String htmlPage, String tabItem) throws Throwable {
-    //     myConn.connect();
-    //         String placeholders = myConn.fetchSql("select placeholderstring from tb910_properties where functionName = ?", tabItem, "placeholderstring");
-    //         String[] placeholder = placeholders.split(";");
-
-    //         htmlPage = htmlPage.replace(placeholder[0], "class='active'");
-    //         for(int t = 1; t < placeholder.length; t++) {
-    //             htmlPage = htmlPage.replace(placeholder[t], "");
-    //         }
-    //     myConn.close();
-    //     return htmlPage;
-    // }
 
 }
--- src/main/java/org/RestServer/RestServer.java
+++ src/main/java/org/RestServer/RestServer.java
@@ -2,15 +2,9 @@ package org.RestServer;
 
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
-
 import javax.swing.*;
-
 import static org.RestServer.FuncsAndProcs.decodeBase64;
-
 import java.sql.SQLException;
-
-import static org.RestServer.FuncsAndProcs.decodeBase64;
-
 
 public class RestServer {
 
@@ -21,16 +15,17 @@ public class RestServer {
             //add public folder to provide static files like css and js
             config.staticFiles.add("/public", Location.CLASSPATH);
             
-            //map get and post routes
+            //map get routes
             config.routes.get("/" , ctx -> ctx.html(getRoot("indexPage")));
-            config.routes.get("/powertrafo" , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
-            config.routes.get("/filterchoke", ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
-            config.routes.get("/outputtrafo", ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
-            config.routes.get("/download"   , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
-            config.routes.get("/search"     , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
-            config.routes.get("/contact"    , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
-            config.routes.get("/about"      , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
-            config.routes.get("/home"       , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
+            config.routes.get("/voedingstrafo" , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
+            config.routes.get("/smoorspoel"    , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
+            config.routes.get("/uitgangstrafo" , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
+            config.routes.get("/weetjes"       , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
+            config.routes.get("/search"        , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
+            config.routes.get("/contact"       , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
+            config.routes.get("/about"         , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
+            config.routes.get("/home"          , ctx -> ctx.html(startSession(ctx.queryParam("ipAddress"), ctx.path().replace("/", ""))));
+            //map post routes
         }).start(7070);
     }
 
