6706326e44adf705ad3dbf21c33ee31d7dad13ad
Roland Labruyere <roland.labruyere@gmail.com>
2026-07-23 00:05:50 +0200

    problems with numberstabel fixed

--- src/main/java/org/RestServer/PowerTrafo.java
+++ src/main/java/org/RestServer/PowerTrafo.java
@@ -19,12 +19,13 @@ public class PowerTrafo  {
     }
 
     public String postPowerTrafoSpecs(String tabItem, String ipAddress, String valueString) throws SQLException {
-        myConn.connect(0); 
+        DbConnect conn = new DbConnect();
+        conn.connect(0); 
         String decodedValues = decodeBase64(valueString);
         String[] value = decodedValues.split("&");
 
         //initialize a new power trafo for this ip 
-        myConn.execSql("insert into tb200_power_trafo_config (ip, trafoNum, timestamp) values (?, ?, ?)", ipAddress + ";" + getNextNumber(tabItem) + ";" + fps.depositTimestamp(0));
+        conn.execSql("insert into voorthuiscustomersales.tb200_power_trafo_config (ip, trafoNum, timestamp) values (?, ?, ?)", ipAddress + ";" + getNextNumber(tabItem) + ";" + fps.depositTimestamp(0));
         return "done";
         }
 
@@ -35,14 +36,14 @@ public class PowerTrafo  {
         myConn.connect(1); 
         String dateString = fps.depositTimestamp(0);
         String mYear = dateString.substring(0, 4);
-        String mMonth = dateString.substring(5, 6);
+        String mMonth = dateString.substring(5, 7);
 
         String trafoNumber = myConn.fetchSql("select * from tb900_numberstabel where itemtype = ?", tabItem, "itemNumber");
         String formattedTrafoNumber =  mYear + mMonth + fps.formatNumber(Integer.parseInt(trafoNumber));
 
         myConn.execSql("delete from tb900_numberstabel where itemType = ?", tabItem);
-        myConn.execSql("insert into tb910_temp_trafo_settings values (?, ?, ?, ?, ?)", tabItem + ";" + mYear + ";" + mMonth + ";" + (Integer.parseInt(trafoNumber) + 1));
-        myConn.close();
+        myConn.execSql("insert into tb900_numberstabel values (?, ?, ?, ?)", tabItem + ";" + mYear + ";" + mMonth + ";" + (Integer.parseInt(trafoNumber) + 1));
+
         return formattedTrafoNumber;
     }
     
